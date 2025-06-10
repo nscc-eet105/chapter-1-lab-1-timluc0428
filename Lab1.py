37d0a7616f64db47ea0584657c867259daf5ac07
Tim Lucas <tlucas@northweststate.edu>
2025-06-09 23:25:32 -0400

    Assignment Complete

---
+++ Lab1.py
@@ -0,0 +1,30 @@
+#storing poem text as variable psalm_23
+psalm_23 = """\nAdonai, my shepherd:
+\tI know no need.
+In lush meadows
+\tyou invite my soul to rest and recline; together\n
+we walk beside the dance of light on quiet waters.
+\tYou give me back myself,
+point me to the road toward justice,
+\tone of your sacred names.\n
+Even when I walk in a valley
+\tdark as the shadow of death,
+I know no fear, for you are near me
+\twith your staff and crook, and I am safe.\n
+You spread out a feast before me
+\twith those who were my enemies.
+You touch my head with fragrant oil;
+\tmy thanks overflows.\n
+May goodness and mercy
+\tfollow me all the days of my life,
+and may I make my home
+\tin the house of the Lord forever."""
+
+#storing full name as variable name
+name = "Tim Lucas\n"
+
+#print Var psalm_23
+print(psalm_23)
+
+#print Var name
+print(name)
