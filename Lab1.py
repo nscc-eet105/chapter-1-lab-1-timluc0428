b1bfba9e66f5059bf1b7c9bb1d0d6c028dc72939
Tim Lucas <tlucas@northweststate.edu>
2025-06-13 14:22:32 -0400

    Assignment fixed

--- Lab1.py
+++ Lab1.py
@@ -1,27 +1,27 @@
 #storing poem text as variable psalm_23
-psalm_23 = """\nAdonai, my shepherd:
-\tI know no need.
-In lush meadows
-\tyou invite my soul to rest and recline; together\n
-we walk beside the dance of light on quiet waters.
-\tYou give me back myself,
-point me to the road toward justice,
-\tone of your sacred names.\n
-Even when I walk in a valley
-\tdark as the shadow of death,
-I know no fear, for you are near me
-\twith your staff and crook, and I am safe.\n
-You spread out a feast before me
-\twith those who were my enemies.
-You touch my head with fragrant oil;
-\tmy thanks overflows.\n
-May goodness and mercy
-\tfollow me all the days of my life,
-and may I make my home
-\tin the house of the Lord forever."""
+psalm_23 = "\nAdonai, my shepherd:\n \
+\tI know no need.\n \
+In lush meadows\n \
+\tyou invite my soul to rest and recline; together\n\n \
+we walk beside the dance of light on quiet waters.\n \
+\tYou give me back myself,\n \
+point me to the road toward justice,\n \
+\tone of your sacred names.\n\n \
+Even when I walk in a valley\n \
+\tdark as the shadow of death,\n \
+I know no fear, for you are near me\n \
+\twith your staff and crook, and I am safe.\n\n \
+You spread out a feast before me\n \
+\twith those who were my enemies.\n \
+You touch my head with fragrant oil;\n \
+\tmy thanks overflows.\n\n \
+May goodness and mercy\n \
+\tfollow me all the days of my life,\n \
+and may I make my home\n \
+\tin the house of the Lord forever."
 
 #storing full name as variable name
-name = "Tim Lucas\n"
+name = "\n   Tim Lucas\n"
 
 #print Var psalm_23
 print(psalm_23)
